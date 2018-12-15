dd4a79ed8f73f9234c13e870c255c2c3331baa6a
Rommel Rico <me@rommelrico.com>
2018-12-15 09:18:03 -0800

    ApprovalCard component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
     return (
@@ -26,6 +27,7 @@ const App = () => {
                 commentText="Woot woot!"
                 avatarURL={faker.image.avatar()}
             />
+            <ApprovalCard />
         </div>
     );
 };
