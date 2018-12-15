8f19f8050c09bdc5c69a87d9a0a1056fc2862b6a
Rommel Rico <me@rommelrico.com>
2018-12-15 09:05:57 -0800

    Passing the avatar URL

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from "faker";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="Nice blog post!" />
-            <CommentDetail author="Alice" timeAgo="Today at 2:00AM" commentText="Yeah!" />
-            <CommentDetail author="Bob" timeAgo="Yesterday at 3:01PM" commentText="Woot woot!" />
+            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="Nice blog post!" avatarURL={faker.image.avatar()} />
+            <CommentDetail author="Alice" timeAgo="Today at 2:00AM" commentText="Yeah!" avatarURL={faker.image.avatar()} />
+            <CommentDetail author="Bob" timeAgo="Yesterday at 3:01PM" commentText="Woot woot!" avatarURL={faker.image.avatar()} />
         </div>
     );
 };
