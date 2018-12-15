45d6eac17d85d76015b663d55cf6776854772ab5
Rommel Rico <me@rommelrico.com>
2018-12-15 09:03:53 -0800

    Passing the comment text

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
-            <CommentDetail author="Alice" timeAgo="Today at 2:00AM" />
-            <CommentDetail author="Bob" timeAgo="Yesterday at 3:01PM" />
+            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="Nice blog post!" />
+            <CommentDetail author="Alice" timeAgo="Today at 2:00AM" commentText="Yeah!" />
+            <CommentDetail author="Bob" timeAgo="Yesterday at 3:01PM" commentText="Woot woot!" />
         </div>
     );
 };
