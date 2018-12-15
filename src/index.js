89017a2e500626d66741c13be2e3819d793273ee
Rommel Rico <me@rommelrico.com>
2018-12-15 09:23:36 -0800

    Passing a CommentDetail prop to the ApprovalCard Component
    Using props.children

--- src/index.js
+++ src/index.js
@@ -7,27 +7,33 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 4:45PM"
-                commentText="Nice blog post!"
-                avatarURL={faker.image.avatar()}
-            />
 
-            <CommentDetail
-                author="Alice"
-                timeAgo="Today at 2:00AM"
-                commentText="Yeah!"
-                avatarURL={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:45PM"
+                    commentText="Nice blog post!"
+                    avatarURL={faker.image.avatar()}
+                />
+            </ApprovalCard>
 
-            <CommentDetail
-                author="Bob"
-                timeAgo="Yesterday at 3:01PM"
-                commentText="Woot woot!"
-                avatarURL={faker.image.avatar()}
-            />
-            <ApprovalCard />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alice"
+                    timeAgo="Today at 2:00AM"
+                    commentText="Yeah!"
+                    avatarURL={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Bob"
+                    timeAgo="Yesterday at 3:01PM"
+                    commentText="Woot woot!"
+                    avatarURL={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
